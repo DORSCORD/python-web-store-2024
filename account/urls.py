6fc895aacbf58db0bf880cc6500209585d014602
DORSCORD <dorscord356@gmail.com>
2024-12-25 19:00:34 +0100

    Add password reset, fix bags

--- account/urls.py
+++ account/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("register/", views.register, name="register"),
     path("edit/", views.edit, name="edit"),
+    # url-адреси для скидання паролю
+    path("password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password-reset-done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password-reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,57 +1,65 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
+from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
 from .models import Profile
 
 # Create your views here.
 
 def user_login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd["username"], password=cd["password"])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse("Успішний вхід на сайт")
+                    return HttpResponse('Успішний вхід на сайт')
+                else:
+                    return HttpResponse('Заборонений акаунт')
             else:
-                return HttpResponse("Забороненний аккаунт")
-        else:
-            return HttpResponse("Несправельний логін чи пароль")
-                
+                return HttpResponse('Неправильний логін чи пароль')
     else:
         form = LoginForm()
-    return render(request, "account/login.html", {"form": form})
+    return render(request, 'account/login.html', {'form': form})
 
 def register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            # Створити новий об'єкт користувача
+            # але поки не зберігати його
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data["password"])
+            # Встановити обраний пароль
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Зберегти об'єкт User
             new_user.save()
+            # Створити профіль користувача
             Profile.objects.create(user=new_user)
-            return render(request, "account/register_done.html", {"new_user": new_user})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, "account/register.html", {"user_form": user_form})
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 @login_required
 def edit(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Профіль було успішно змінено")
+        else:
+            messages.error(request, "Помилка оновлення профілю")
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.profile)
-    return render(request, "account/edit.html", {
-        "user_form": user_form,
-        "profile_form": profile_form,
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
     })
